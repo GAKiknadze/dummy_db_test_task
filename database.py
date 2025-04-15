a45fb86a1bb6c8cd511d6e258209f814cff16eae
GAKiknadze <gakiknadze@yandex.ru>
2025-04-15 18:13:04 +0300

    Fixed bug with old counts

--- database.py
+++ database.py
@@ -1,93 +1,137 @@
-from typing import Dict, List, Optional
+from collections import defaultdict
+from typing import Dict, List, Optional, Set
 
 
 class Database:
-    main_data: Dict[str, str]
-    transaction_stack: List[Dict[str, str]]
-    value_counts: Dict[str, int]
-    
-    def __init__(self):
-        self.main_data = dict()
-        self.transaction_stack = list()
-        self.value_counts = dict()
+    __main_data: Dict[str, str]
+    __transaction_stack: List[Dict[str, Optional[str]]]
+    __value_counts: Dict[str, int]
 
-    def _get_current_transaction(self) -> Optional[Dict[str, str]]:
-        return self.transaction_stack[-1] if self.transaction_stack else None
+    def __init__(self):
+        self.__main_data = {}
+        self.__transaction_stack = []
+        self.__value_counts = defaultdict(int)
+
+    def _get_current_transaction(self) -> Optional[Dict[str, Optional[str]]]:
+        return self.__transaction_stack[-1] if self.__transaction_stack else None
 
     def set(self, key: str, value: str) -> None:
         current = self._get_current_transaction()
-        source: Dict[str, str]
+        source = current if current is not None else self.__main_data
 
-        if current is not None:
-            source = current
-        else:
-            source = self.main_data
+        old_value = self.get(key)
+        if old_value == value:
+            return
 
-        old_value = source.get(key)
         if old_value is not None:
-            self.value_counts[old_value] -= 1
+            self.__value_counts[old_value] -= 1
+            if self.__value_counts[old_value] == 0:
+                del self.__value_counts[old_value]
+
         source[key] = value
-        self.value_counts[value] = self.value_counts.get(value, 0) + 1
+        self.__value_counts[value] += 1
 
     def get(self, key: str) -> Optional[str]:
-        for transaction in reversed(self.transaction_stack):
+        for transaction in reversed(self.__transaction_stack):
             if key in transaction:
-                return transaction[key]
-        return self.main_data.get(key)
+                return transaction[key] if transaction[key] is not None else None
+        return self.__main_data.get(key)
 
     def unset(self, key: str) -> None:
         current = self._get_current_transaction()
+
         if current is not None:
-            current.pop(key, None)
+            old_value = self.get(key)
+            current[key] = None
+            if old_value is not None:
+                self.__value_counts[old_value] -= 1
+                if self.__value_counts[old_value] == 0:
+                    del self.__value_counts[old_value]
         else:
-            if key in self.main_data:
-                value = self.main_data.pop(key)
-                self.value_counts[value] -= 1
+            old_value = self.__main_data.get(key)
+            if old_value is not None:
+                del self.__main_data[key]
+                self.__value_counts[old_value] -= 1
+                if self.__value_counts[old_value] == 0:
+                    del self.__value_counts[old_value]
 
     def counts(self, value: str) -> int:
-        return self.value_counts.get(value, 0)
+        return self.__value_counts.get(value, 0)
 
     def find(self, value: str) -> Optional[List[str]]:
         result = []
-        all_data = [self.main_data] + self.transaction_stack
-        for data in all_data:
-            for k, v in data.items():
-                if v == value and k not in result:
-                    result.append(k)
+        seen: Set[str] = set()
+
+        keys = set()
+        for transaction in self.__transaction_stack:
+            keys.update(transaction.keys())
+        keys.update(self.__main_data.keys())
+
+        for key in keys:
+            current_value = self.get(key)
+            if current_value == value and key not in seen:
+                result.append(key)
+                seen.add(key)
+
         return sorted(result) if result else None
 
     def begin(self) -> None:
-        self.transaction_stack.append(dict())
+        self.__transaction_stack.append({})
 
     def rollback(self) -> bool:
-        if self.transaction_stack:
-            transaction = self.transaction_stack.pop()
-            for _, value in transaction.items():
-                if value is not None:
-                    self.value_counts[value] -= 1
-            return True
-        return False
-
-    def commit(self) -> bool:
-        if not self.transaction_stack:
+        if not self.__transaction_stack:
             return False
 
-        current_transaction = self.transaction_stack.pop()
+        transaction = self.__transaction_stack.pop()
 
-        if self.transaction_stack:
-            parent = self.transaction_stack[-1]
-            for key, value in current_transaction.items():
-                parent[key] = value
-        else:
-            for key, value in current_transaction.items():
-                if value is None:
-                    if key in self.main_data:
-                        old_value = self.main_data.pop(key)
-                        self.value_counts[old_value] -= 1
-                else:
-                    old_value = self.main_data.get(key)
-                    if old_value is not None:
-                        self.value_counts[old_value] -= 1
-                    self.main_data[key] = value
-                    self.value_counts[value] = self.value_counts.get(value, 0) + 1
+        for key, value in transaction.items():
+            if value is not None:
+                self.__value_counts[value] -= 1
+                if self.__value_counts[value] == 0:
+                    del self.__value_counts[value]
+
+                restored_value = self.get(key)
+                if restored_value is not None:
+                    self.__value_counts[restored_value] += 1
+            else:
+                restored_value = self.get(key)
+                if restored_value is not None:
+                    self.__value_counts[restored_value] += 1
+        return True
+
+    def commit(self) -> bool:
+        if not self.__transaction_stack:
+            return False
+
+        current = self.__transaction_stack.pop()
+        target = (
+            self.__transaction_stack[-1]
+            if self.__transaction_stack
+            else self.__main_data
+        )
+
+        for key, value in current.items():
+            if value is None:
+                old_value = target.get(key)
+                if old_value is not None:
+                    self.__value_counts[old_value] -= 1
+                    if self.__value_counts[old_value] == 0:
+                        del self.__value_counts[old_value]
+                    del target[key]
+            else:
+                old_value = target.get(key)
+                if old_value == value:
+                    continue
+
+                if old_value is not None:
+                    self.__value_counts[old_value] -= 1
+                    if self.__value_counts[old_value] == 0:
+                        del self.__value_counts[old_value]
+
+                target[key] = value
+                self.__value_counts[value] += 1
+
+        if self.__transaction_stack:
+            self.__transaction_stack[-1].update(target)
+
         return True
